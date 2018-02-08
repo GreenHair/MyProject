a5b3eb86b9a304f83aa09fe55d0917241d925e8a
Auke Swen <aukeswen2001@yahoo.com>
2018-02-08 10:03:27 +0100

    Entwurf Eintrag

---
+++ Haushaltsbuch/Eintrag.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace Haushaltsbuch
+{
+    class Eintrag
+    {
+        public static StackPanel NeuerRechnung(List<Shop> Laeden, List<Produktgruppe> Prodgr, List<Person> Familie)
+        {
+            StackPanel stackpanel = new StackPanel();
+            stackpanel.Children.Add(new Label
+            {
+                Content = "Neuer Rechnung",
+                FontSize = 18,
+                FontWeight = FontWeights.DemiBold,
+                HorizontalAlignment = HorizontalAlignment.Center
+            });
+            StackPanel ZeileRechnung = new StackPanel { Orientation = Orientation.Horizontal };
+            ZeileRechnung.Children.Add(new DatePicker { SelectedDate = DateTime.Now.Date });
+            ZeileRechnung.Children.Add(new ComboBox { ItemsSource = Laeden });
+            ZeileRechnung.Children.Add(new ComboBox { ItemsSource = Familie });
+            ZeileRechnung.Children.Add(new CheckBox { Content = "Einmalig", IsChecked = true });
+            stackpanel.Children.Add(ZeileRechnung);
+            stackpanel.Children.Add(new Label
+            {
+                Content = "Rechnungsposten",
+                FontSize = 18,
+                FontWeight = FontWeights.DemiBold,
+                HorizontalAlignment = HorizontalAlignment.Center
+            });
+            for (int i = 0; i < 20; i++)
+            {
+                StackPanel ZeilePosten = new StackPanel { Orientation = Orientation.Horizontal };
+                ZeilePosten.Children.Add(new TextBox { Width = 200 });
+                ZeilePosten.Children.Add(new TextBox { Width = 100 });
+                ZeilePosten.Children.Add(new ComboBox { ItemsSource = Prodgr });
+                stackpanel.Children.Add(ZeilePosten);
+            }
+            return stackpanel;
+        }
+    }
+}
--- Haushaltsbuch/MainWindow.xaml.cs
+++ Haushaltsbuch/MainWindow.xaml.cs
@@ -47,13 +47,16 @@ namespace Haushaltsbuch
 
         private void Uebersicht_Click(object sender, RoutedEventArgs e)
         {
-            grdEinkommen.Visibility = Visibility.Collapsed;
             scrlbar.Visibility = Visibility.Visible;
+            grdEinkommen.Visibility = Visibility.Collapsed;
+            tbcEintrag.Visibility = Visibility.Collapsed;            
         }
 
         private void Eintrag_Click(object sender, RoutedEventArgs e)
         {
-
+            tbcEintrag.Visibility = Visibility.Visible;
+            scrlbar.Visibility = Visibility.Collapsed;
+            grdEinkommen.Visibility = Visibility.Collapsed;            
         }
 
         private void Window_Loaded(object sender, RoutedEventArgs e)
@@ -64,17 +67,8 @@ namespace Haushaltsbuch
             var PrevMonEin = from einnahmen in myHaushaltsbuch.einnahmen where einnahmen.Datum.Month == DateTime.Now.Month -1 select einnahmen;
             UebersichtEinkommen("Einkommen vorherigen Monat", stckEinkommenPrev, PrevMonEin);
 
-            //myHaushaltsbuch = new Hauptbuch();
-            //this_week = new Anzeige(myHaushaltsbuch.dieseWoche, false);
-            //last_week = new Anzeige(myHaushaltsbuch.LetzteWoche, false);
-            //this_month = new Anzeige(myHaushaltsbuch.monatsliste(0), true);
-            //last_month = new Anzeige(myHaushaltsbuch.monatsliste(1), true);
-            //thisWeek.Content = this_week.Dashboard;
-            //lastWeek.Content = last_week.Dashboard;
-            //thisMonth.Content = this_month.Dashboard;
-            //lastMonth.Content = last_month.Dashboard;
-            //if (thisWeek.Focus()) ;
-            // MessageBox.Show("ThisWeek hat focus");
+            tbiRechnung.Content = Eintrag.NeuerRechnung(myHaushaltsbuch.)
+
             this_week.diagrammAnimiert();
             
         }
@@ -97,6 +91,7 @@ namespace Haushaltsbuch
         {
             grdEinkommen.Visibility = Visibility.Visible;
             scrlbar.Visibility = Visibility.Collapsed;
+            tbcEintrag.Visibility = Visibility.Collapsed;
         }
 
         private void Suchen_CLick(object sender, RoutedEventArgs e)
